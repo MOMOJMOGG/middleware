d1e82c74028b4223f6e157742fa817f7c5aebd60
MOMOJMOGG <momomacaca@gmail.com>
2021-05-17 22:39:29 +0800

    feat: add logger to display and write logs file

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 // app.js
 const express = require('express')
+const moment = require('moment')
+const fs = require('fs')
+const chalk = require('chalk')
 const app = express()
 const port = 3000
 
-app.use((req, res, next) => {
-  console.log('[Logs] Time:', Date.now(), ' | ', req.method, 'from', req.originalUrl)
+
+let logger = (req, res, next) => {
+  let date = new Date()
+  date = moment(date).format('YYYY-MM-DD hh:mm:ss')
+  const method = req.method
+  const url = req.url
+  const status = res.statusCode
+  const log = `${chalk.redBright("[Logs]")} ${chalk.blue(date)} | ${method} from ${url} ${chalk.green(status)}`
+  console.log(log)
+  const logContent = `[Logs] ${date} | ${method} from ${url} ${status}`
+  fs.appendFile("./Logs/request.txt", logContent + "\n", err => {
+    if (err) console.log(err)
+  })
   next()
-})
+}
+
+app.use(logger)
 
 app.get('/', (req, res) => {
   res.send('列出全部 Todo')
